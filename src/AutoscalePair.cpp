b05187bb36cf16655bce649491c513e75da0d788
flyfuri <93001340+flyfuri@users.noreply.github.com>
2024-03-03 15:45:59 +1300

    fixes in new class AutoscalePair
    - constructors changed to public
    - call update scaling Factors
    - reset tempMin and tempMax when min or max found
    - add debug function

--- src/AutoscalePair.cpp
+++ src/AutoscalePair.cpp
@@ -1,4 +1,7 @@
 #include "AutoscalePair.h"
+#include <arduino.h>
+
+
 template <typename T> CAutoScalePair<T>::CAutoScalePair(){
     targetMax = 150;
     targetMin = 50;
@@ -10,14 +13,16 @@ template <typename T> CAutoScalePair<T>::CAutoScalePair(T targMin, T targMax){
 }
 
 template <typename T> bool CAutoScalePair<T>::update(T *inChA, T *inChB, T *outA, T *outB){
+    m__chA.lastVal = m__chA.actVal;
+    m__chB.lastVal = m__chB.actVal;
     m__chA.actVal = *inChA;
     m__chB.actVal = *inChB;
-    searchMinMax(&m__chA, &m__chB);
-    searchMinMax(&m__chB, &m__chA);
+    searchMinMax(&m__chA);
+    searchMinMax(&m__chB);
     updateMinMaxAverage(&m__chA, &m__chB);   
     updateMinMaxAverage(&m__chB, &m__chA);
-
-
+    updateScalingFactors(&m__chA);
+    updateScalingFactors(&m__chB);
     scaleCH(&m__chA, outA);
     scaleCH(&m__chB, outB);
     if (m__chA.mode == 1 && m__chB.mode == 1){
@@ -26,30 +31,40 @@ template <typename T> bool CAutoScalePair<T>::update(T *inChA, T *inChB, T *outA
     return false;
 } 
 
-template <typename T> void CAutoScalePair<T>::searchMinMax(Ch *ChToChk, Ch *ChRef){
-    if (ChToChk->actVal > ChToChk->lastVal && ChToChk->risingIndex < 5){
-        if (ChToChk->risingIndex >=0){ChToChk->risingIndex++;}
-        else{ChToChk->risingIndex = 0;}
+template <typename T> void CAutoScalePair<T>::searchMinMax(Ch *ChToChk){
+    if (ChToChk->actVal > ChToChk->lastVal){
+        if (ChToChk->risingIndex >= 0){ChToChk->risingIndex++;}
+        else{ChToChk->risingIndex = 1;}
     }
-    else if (ChToChk->actVal < ChToChk->lastVal&& ChToChk->risingIndex > -5){
-        if (ChToChk->risingIndex <=0){ChToChk->risingIndex--;}
-        else{ChToChk->risingIndex = 0;}
+    else if (ChToChk->actVal < ChToChk->lastVal){
+        if (ChToChk->risingIndex <= 0){ChToChk->risingIndex--;}
+        else{ChToChk->risingIndex = -1;}
     }
-    else if (ChToChk->actVal == ChToChk->lastVal){
-        ChToChk->risingIndex = 0;
-    }
+
     if (ChToChk->actVal > ChToChk->tempMax){ChToChk->tempMax = ChToChk->actVal;}
     if (ChToChk->actVal < ChToChk->tempMin){ChToChk->tempMin = ChToChk->actVal;}
-    ChToChk->lastVal = ChToChk->actVal;
+    T valueChange = ChToChk->actVal - ChToChk->lastVal;
+    ChToChk->FiltValueChanges.measurement(valueChange);
+    ChToChk->SumValChangeLastX = ChToChk->FiltValueChanges.getSum();
+    valueChange = abs(valueChange);
+    ChToChk->FiltValueChangesAbsolut.measurement(valueChange);
+    ChToChk->SumAbsValChangeLastX = ChToChk->FiltValueChangesAbsolut.getSum();
+    
 }
 
 template <typename T> void CAutoScalePair<T>::updateMinMaxAverage(Ch *ChToChk, Ch *ChRef){
-   if (ChRef->risingIndex > 2 || ChRef->risingIndex < -2){
+   if (ChRef->SumAbsValChangeLastX > 10 && ChRef->SumAbsValChangeLastX < 25 && (ChRef->risingIndex > 6 || ChRef->risingIndex < -6)){
         if (ChToChk->risingIndex == 3 ){
+            ChToChk->risingIndex++; //avoid taking same valley twice
             ChToChk->avMin = ChToChk->FiltAverMin.measurement(ChToChk->tempMin);
+            ChToChk->tempMin = DEFAULT_MIN;
+            ChToChk->tempMax = DEFAULT_MAX;
         }
-        if (ChToChk->risingIndex == 3){
-            ChToChk->avMax = ChToChk->FiltAverMax.measurement(ChToChk->tempMax); 
+        if (ChToChk->risingIndex == -3){
+            ChToChk->avMax = ChToChk->FiltAverMax.measurement(ChToChk->tempMax);
+            ChToChk->risingIndex--; //avoid taking same peak twice
+            ChToChk->tempMin = DEFAULT_MIN;
+            ChToChk->tempMax = DEFAULT_MAX; 
         }
     }
 }
@@ -57,21 +72,48 @@ template <typename T> void CAutoScalePair<T>::updateMinMaxAverage(Ch *ChToChk, C
 template <typename T> void CAutoScalePair<T>::updateScalingFactors(Ch *ChToChk){
     if (ChToChk->FiltAverMax.getNbrMeas() > 5 && ChToChk->FiltAverMin.getNbrMeas()){
         ChToChk->mode = 1;
-        ChToChk->minLevel = ChToChk->avMin;
-        ChToChk->factor = targetMax/(ChToChk->avMax - ChToChk->minLevel);
+        ChToChk->minLevelOffs = targetMin - ChToChk->avMin;
+        ChToChk->factor = (float)targetMax/(float)(ChToChk->avMax - ChToChk->avMin);
     }
 }
 
 template <typename T> void CAutoScalePair<T>::scaleCH(Ch *ChToScale, T *ChOut){
     //amplifying channels
-    *ChOut = (ChToScale->actVal - ChToScale->minLevel) * ChToScale->factor + ChToScale->minLevel;
+    *ChOut = (ChToScale->actVal - ChToScale->avMin) * ChToScale->factor + ChToScale->avMin + ChToScale->minLevelOffs;
 } 
 
-template <typename T> void CAutoScalePair<T>::setScalingFactors(T Ch1_fact, T Ch2_fact, T Ch1_minLev, T Ch2_minLev){
-    m__chA.factor = Ch1_fact;
-    m__chB.factor = Ch2_fact;
-    m__chA.minLevel = Ch1_minLev;
-    m__chB.minLevel = Ch2_minLev;
+template <typename T> void CAutoScalePair<T>::setScalingFactors(T Ch1_fact, T Ch2_fact, T Ch1_minLevOffs, T Ch2_minLevOffs){
+    m__chA.factor = (float)Ch1_fact;
+    m__chB.factor = (float)Ch2_fact;
+    m__chA.minLevelOffs = Ch1_minLevOffs;
+    m__chB.minLevelOffs = Ch2_minLevOffs;
+}
+
+template <typename T> bool CAutoScalePair<T>::debugCh(bool ChB, float *result_arr, int size_arr){
+    Ch *ChToDbg;
+    if (ChB){
+        ChToDbg = &m__chB;
+    }
+    else {
+        ChToDbg = &m__chA;
+    }
+
+    if (size_arr >= 12){
+        *result_arr = ChToDbg->tempMin;
+        *(result_arr + 1) = ChToDbg->tempMax;
+        *(result_arr + 2) = ChToDbg->actVal;
+        *(result_arr + 3) = ChToDbg->lastVal;
+        *(result_arr + 4) = ChToDbg->avMin;
+        *(result_arr + 5) = ChToDbg->avMax;
+        *(result_arr + 6) = ChToDbg->minLevelOffs;
+        *(result_arr + 7) = ChToDbg->factor;
+        *(result_arr + 8) = ChToDbg->mode;
+        *(result_arr + 9) = ChToDbg->risingIndex;
+        *(result_arr + 10) = ChToDbg->SumAbsValChangeLastX;
+        *(result_arr + 11) = ChToDbg->SumValChangeLastX;
+        return true;
+    }
+    return false;
 }
 
 template class CAutoScalePair<int>;
--- src/AutoscalePair.h
+++ src/AutoscalePair.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #define DEFAULT_MAX -9999
 #define DEFAULT_MIN 9999
 
@@ -8,28 +9,31 @@
 template <typename T> 
 class CAutoScalePair{
 private: 
-    T targetMax = 150, targetMin = 50; //target size of sinoide defined my valley(min) and peaks(max)
+    T targetMax = 100, targetMin = 0; //target size of sinoide defined my valley(min) and peaks(max)
     struct Ch{
         T tempMax = DEFAULT_MAX, tempMin = DEFAULT_MIN; //min max on actual curve section
         T actVal= 0; //actual value 
         T lastVal = 0;  // prevoius value
+        T avMin = DEFAULT_MIN, avMax = DEFAULT_MAX; //actual average min/max 
+        T minLevelOffs = 0;  //level of minimum(above 0) 
+        float factor = 1; //factor to scale the channel
         int mode = 0; //0= init, 1 = scaling
         int risingIndex; //value from -3 to 3 meaning how many consecutive times the value raised or fell
+        T SumAbsValChangeLastX, SumValChangeLastX;  //
         ANFLTR::CFilterAnalogOverMeasures<T> FiltAverMin{10,10}, FiltAverMax{10,10}; //filter to calculate rolling average min and max of signal
-        T avMin = DEFAULT_MIN, avMax = DEFAULT_MAX; //actual average min/max 
-        T factor = 1; //factor to scale the channel
-        T minLevel = 0;  //level of minimum(above 0) 
+        ANFLTR::CFilterAnalogOverMeasures<T> FiltValueChangesAbsolut{5,5}, FiltValueChanges{5,5};
     }m__chA, m__chB;
-    CAutoScalePair();
-    CAutoScalePair(T targMin, T targMax);
     void scaleCH(Ch *ChToScale, T *ChOut);          
-    void searchMinMax (Ch *ChToChk, Ch *ChRef);
+    void searchMinMax (Ch *ChToChk);
     void updateMinMaxAverage(Ch *ChToChk, Ch *ChRef);
     void updateScalingFactors(Ch *ChtoChk);
-public:
-    
+    bool debugChX(Ch *ChToDbg, float *result_arr, int size_arr12);
+public: 
+    CAutoScalePair();
+    CAutoScalePair(T targMin, T targMax);
     void setScalingFactors(T Ch1_fact, T Ch2_fact, T Ch1_minLev, T Ch2_minLev);
-    bool update(T *inChA, T *inChB, T *outA, T *outB);  
-
+    bool update(T *inChA, T *inChB, T *outA, T *outB); 
+    bool debugChA(float *result_arr, int size_arr12);
+    bool debugChB(float *result_arr, int size_arr12);
 };
 
