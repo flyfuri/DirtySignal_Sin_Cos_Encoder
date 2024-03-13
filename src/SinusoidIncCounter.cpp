a62d0926c04f98cf34a15ca9d503d98468f871cc
flyfuri <93001340+flyfuri@users.noreply.github.com>
2024-03-13 16:39:09 +1300

    version with average min max interplating cleaned from old stuff

--- src/SinusoidIncCounter.cpp
+++ src/SinusoidIncCounter.cpp
@@ -8,13 +8,8 @@
 
 #define _USE_MATH_DEFINES
 
-#ifndef DEBGOUT
-    #define DEBGOUT 0  //if this is 99 dbugprints will be active
-#endif
-
 CSinIncCntr::CSinIncCntr(){
-    m__sumOnLastCrossing = -9999; 
-    m__sumMidLine = 0;
+    m__sumMidLine = 80;
     m__actStatusSUB = 0;  
     m__actStatusSUM = 0; 
     m__offset = 0;
@@ -25,7 +20,7 @@ int CSinIncCntr::setSumMidLine(int midl){
 }
 
 int CSinIncCntr::m__SinInterpolMinMax(int min, int max, int actval, int resolution){
-    if(max > 0 && min > 0){ //do only interpolate if some min and max is memorised already
+    if(max - min > 20){ 
         if(actval < min){
             return 0;
         }
@@ -34,11 +29,6 @@ int CSinIncCntr::m__SinInterpolMinMax(int min, int max, int actval, int resoluti
         }
         else{
             double tmpresult = (resolution / PI) * ((asin(((2 / ((float)max - (float)min)) * ((float)actval - (float)min)) - 1)) + PI / 2);
-
-            /*double tmpmax = max -min;  
-            double tmpactval = actval - min;
-            double tmpresult = (resolution / PI) * ((asin(((2 / tmpmax) * tmpactval)-1)) + PI/2);
-            //double tmpresult = (resolution/(PI/2)) * sin(((PI/2)/tmpmax) * tmpactval);*/
             return (int)(tmpresult * 1000) % 1000 >= 500 ? (int)tmpresult + 1 : (int)tmpresult;  //take 3 digits after period to round
         }
     }
@@ -67,42 +57,32 @@ int CSinIncCntr::calc(int actCh1, int actCh2){
     }
 
     if(m__actStatusSUB > 0 && m__sub < 0){ //CROSSING: when difference is crossing Null-line from positive to negative
-        if (m__sumMidLine != 0){
-            if(m__sum >= m__sumMidLine) //sub FALLING sum at MAX  (channel lines are crossing: sub-curve crossing nullpoint FALLING with summary at MAX)
-            {   
-                m__intpolMax = SumCurveLastMaxs.measurement(m__sum);
-                m__actHalfTooth--;
-            }
-            else //sub FALLING sum at MIN (channel lines are crossing: sub-curve crossing nullpoint FALLING with summary at MIN)
-            {
-                m__intpolMin = SumCurveLastMins.measurement(m__sum);
-                m__actHalfTooth++;
-            } 
+        if(m__sum >= m__sumMidLine) //sub FALLING sum at MAX  (channel lines are crossing: sub-curve crossing nullpoint FALLING with summary at MAX)
+        {   
+            m__intpolMax = SumCurveLastMaxs.measurement(m__sum);
+            m__actHalfTooth--;
         }
-        m__sumOnLastCrossing = m__sum;
+        else //sub FALLING sum at MIN (channel lines are crossing: sub-curve crossing nullpoint FALLING with summary at MIN)
+        {
+            m__intpolMin = SumCurveLastMins.measurement(m__sum);
+            m__actHalfTooth++;
+        } 
         m__actStatusSUB = -1;  //always to do when sub crossing zero line!
     }  
     else if(m__actStatusSUB < 0 && m__sub >= 0){////CROSSING: when difference is crossing Null-line from negative to positive
-        if (m__sumMidLine != 0){
-            if(m__sum >= m__sumMidLine)//sub RISING sum at MAX  (channel lines are crossing: sub-curve crossing nullpoint RISING with summary at MAX)
-            {   
-                m__intpolMax = SumCurveLastMaxs.measurement(m__sum);
-                m__actHalfTooth++;
-            }
-            else //sub RISING sum at MIN  (channel lines are crossing: sub-curve crossing nullpoint RISING with summary at MIN)
-            {
-                m__intpolMin = SumCurveLastMins.measurement(m__sum);
-                m__actHalfTooth--;
-            }
+       if(m__sum >= m__sumMidLine)//sub RISING sum at MAX  (channel lines are crossing: sub-curve crossing nullpoint RISING with summary at MAX)
+        {   
+            m__intpolMax = SumCurveLastMaxs.measurement(m__sum);
+            m__actHalfTooth++;
         }
-        m__sumOnLastCrossing = m__sum;
+        else //sub RISING sum at MIN  (channel lines are crossing: sub-curve crossing nullpoint RISING with summary at MIN)
+        {
+            m__intpolMin = SumCurveLastMins.measurement(m__sum);
+            m__actHalfTooth--;
+        }
         m__actStatusSUB = 1; //always to do when sub crossing zero line!
     }
-    /*else{
-        m__intpolMax = toothedrack[NBR_TEETH_ON_RACK + m__actHalfTooth].maxAv;
-        m__intpolMin = toothedrack[NBR_TEETH_ON_RACK + m__actHalfTooth].minAv;
-    } */
-    if (m__sumMidLine != 0){
+    if (m__intpolMax - m__intpolMin > 20){  //only interplate when min and max differ
         int tempIntpol = m__SinInterpolMinMax(m__intpolMin, m__intpolMax, m__sum, INTPOLRES);
 
         int tmpActPos;
@@ -130,23 +110,17 @@ int CSinIncCntr::setTo(int value){
     return m__offset = value - (m__actPos - m__offset);
 } 
 
-bool CSinIncCntr::debug(float *result_arr, int size_arr15){
-    if (size_arr15 >= 15){
+bool CSinIncCntr::debug(float *result_arr, int size_arr9){
+    if (size_arr9 >= 9){
         *result_arr = m__sum;
         *(result_arr + 1) = m__sub;
-        *(result_arr + 2) = m__sumOnLastCrossing;
-        *(result_arr + 3) = m__sumLowestMax;
-        *(result_arr + 4) = m__sumHighestMin;
-        *(result_arr + 5) = m__actStatusSUM;
-        *(result_arr + 6) = m__actStatusSUB;
-        *(result_arr + 7) = m__sumAtPowerON;
-        *(result_arr + 8) = m__offset;
-        *(result_arr + 9) = m__actIndexTeethrack;
-        *(result_arr + 10) = m__actIndexTeethrack;
-        *(result_arr + 11) = m__intpolMax;
-        *(result_arr + 12) = m__intpolMin;
-        *(result_arr + 13) = m__actHalfTooth;
-        *(result_arr + 14) = m__actPos;
+        *(result_arr + 2) = m__actStatusSUM;
+        *(result_arr + 3) = m__actStatusSUB;
+        *(result_arr + 4) = m__offset;
+        *(result_arr + 5) = m__intpolMax;
+        *(result_arr + 6) = m__intpolMin;
+        *(result_arr + 7) = m__actHalfTooth;
+        *(result_arr + 8) = m__actPos;
         return true;
     }
     return false;
--- src/SinusoidIncCounter.h
+++ src/SinusoidIncCounter.h
@@ -5,33 +5,22 @@
 
 
 #define INTPOLRES 100 //resulution of interpolation between halfteeth
-#define INIT_MIN_DIST_SUM_MINMAX 70 //initial minimal dist
-#define SUM_MIDLINE_ABOVE_MIN 25 //initial minimal dist
-
 
 class CSinIncCntr{
     private: 
         //used for basic counting the theeth (counts twice per tooth on each crossing point of the 2 channel curves):
         int m__sum;  //summary of both channels
         int m__sub;   //CH1 - CH2
-        int m__sumOnLastCrossing = -9999; //point on sum curve when channel curves crosse last time
-        int m__sumLowestMax;  //defines middle line of summary 
-        int m__sumHighestMin; //defines middle line of summary 
         int m__sumMidLine = 0; //approx middle line of summary
         short m__actStatusSUM = 0;  //wich half of the summary curve we're now (-1=MIN, 0 undefined(at beginning), 1=MAX)
         short m__actStatusSUB = 0;  //wich half of the difference curve we're now (-1=negative, 0 undefined(at beginning), 1=positive)
-        int m__sumAtPowerON = -9999; //take 
-        int m__offset = -9999; //offset calculated after very first travel beetween to opposite crossings of the channel curve
+
+        int m__offset = 0; //offset for end result
         
-        // used to find the approx. middle line of summary curve
+        // used to find the average peek and valley to be used for interplation
         ANFLTR::CFilterAnalogOverMeasures<int> SumCurveLastMaxs{10,10};
-        ANFLTR::CFilterAnalogOverMeasures<int> SumCurveLastMins{10,10};
-        //bool m__initialSumMidFound; //initial sum middle line has been found
+        ANFLTR::CFilterAnalogOverMeasures<int> SumCurveLastMins{10,10};  
 
-        //used for interpolation beetween flank counts to increse resolution 
-        //CTeethMemory<int> TeethMem_Sum;   
-
-        int m__actIndexTeethrack; 
         int m__intpolMax;  //actual Max used for interpolation
         int m__intpolMin;    //actual Min used for interpolation
 
